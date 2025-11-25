5c2ef2278894c11791883c249967e7e51b9b2dd9
Pavel Piryutko <pavelpryutko@yandex.ru>
2025-11-25 11:27:25 +0300

    Test saveURL functional

--- internal/storage/sqllite/sqllite.go
+++ internal/storage/sqllite/sqllite.go
@@ -3,6 +3,9 @@ package sqllite
 import (
 	"database/sql"
 	"fmt"
+	"url-shortner/internal/storage"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 type Storage struct {
@@ -10,10 +13,51 @@ type Storage struct {
 }
 
 func New(storagePath string) (*Storage, error) {
-	const op = "storage.sqllite.New"
+	const op = "storage.sqlite.New"
 
-	db, err := sql.Open("sqllite3", "./url-shortner.db")
+	db, err := sql.Open("sqlite3", storagePath)
 	if err != nil {
-		return nil, fmt.Errorf("%s, %w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+	stmt, err := db.Prepare(`
+        CREATE TABLE IF NOT EXISTS url(
+            id INTEGER PRIMARY KEY,
+            alias TEXT NOT NULL UNIQUE,
+            url TEXT NOT NULL
+        );
+        CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
+    `)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUrlExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: failed to get last insert id: %w", op, err)
+	}
+	return id, nil
 }
